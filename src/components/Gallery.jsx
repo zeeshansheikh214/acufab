3ab6156a039b4fb756c9110d45a28caa0abf9329
zeeshansheikh214 <wm.inspyre@gmail.com>
2023-10-10 12:40:48 -0700

    create gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,7 +12,6 @@ import {
   second_gallery_1,
   second_gallery_2,
   second_gallery_3,
-  ser_1,
   third_gallery_1,
   third_gallery_2,
   third_gallery_3,
@@ -25,43 +24,32 @@ import { Fragment, useState } from "react";
 
 const Gallery = () => {
 
-    // const [indexValue,setIndexValue]=useState(0);
   const myarr = [
     {
       name: "Roofing",
       img1: roofing1,
-      img:[
-        roofing2,
-        roofing3
-      ]
+      img: [roofing2, roofing3],
     },
-
     {
       name: "second gallery",
       img1: second_gallery_1,
-      img:[
-        second_gallery_2,second_gallery_3
-      ]
+      img: [second_gallery_2, second_gallery_3],
     },
     {
       name: "third gallery",
       img1: third_gallery_1,
-      img:[
-        third_gallery_2,
-        third_gallery_3
-        
-      ]
+      img: [third_gallery_2, third_gallery_3, third_gallery_4, third_gallery_5],
     },
     {
       name: "fourth gallary",
       img1: fourth_gallary_1,
-      img:[
+      img: [
         fourth_gallary_2,
         fourth_gallary_3,
         fourth_gallary_4,
         fourth_gallary_5,
-        fourth_gallary_6
-      ]
+        fourth_gallary_6,
+      ],
     },
   ];
   const settings = {
@@ -101,13 +89,29 @@ const Gallery = () => {
     ],
   };
   let [isOpen, setIsOpen] = useState(false);
+  
 
   function closeModal() {
     setIsOpen(false);
+    setSelectedIndex(null);
+    setZoomedInImage(null);
   }
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  const [zoomedInImage, setZoomedInImage] = useState(null);
+  
 
-  function openModal() {
+  function openModal(index) {
     setIsOpen(true);
+    setSelectedIndex(index);
+    // setZoomedInImage(index);
+    
+  }
+  function openModal2(index) {
+    // setIsOpen(true);
+    // setSelectedIndex(index);
+    setZoomedInImage(index);
+    // console.log(zoomedInImage);
+    
   }
   return (
     <div>
@@ -125,99 +129,135 @@ const Gallery = () => {
         </div>
         <div className="container mx-auto ser-slider">
           <Slider {...settings}>
-            {myarr.map((ser,index) => {
+            {myarr.map((index, key) => {
               return (
                 <div className="max-w-[400px] relative">
-                  <Image src={ser.img1} width={400} height={600} alt="service 1" />
+                  <Image
+                    src={index.img1}
+                    width={400}
+                    height={600}
+                    alt="service 1"
+                  />
                   <div className="content py-4 text-center px-3 absolute bottom-0 left-0 right-0">
-                    <button
-                      type="button"
-                      onClick={openModal}
-                      className="px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-md hover:bg-opacity-50 duration-300 shadow-xl"
-                    >
-                      {ser.name}
+                    <button type="button" onClick={() => openModal(index)} className="inline-flex justify-center px-4 py-2 text-sm text-white bg-red-700 border border-transparent rounded-md hover:bg-red-500 duration-300">
+                      {index.name}
                     </button>
                   </div>
-                  <Transition appear show={isOpen} as={Fragment}>
-                    <Dialog
-                      as="div"
-                      className="fixed inset-0 z-10 overflow-y-auto"
-                      onClose={closeModal}
-                    >
-                      <div className="min-h-screen px-4 text-center">
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0"
-                          enterTo="opacity-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100"
-                          leaveTo="opacity-0"
-                        >
-                          <Dialog.Overlay className="fixed inset-0" />
-                        </Transition.Child>
-
-                        {/* This element is to trick the browser into centering the modal contents. */}
-                        <span
-                          className="inline-block h-screen align-middle"
-                          aria-hidden="true"
-                        >
-                          &#8203;
-                        </span>
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0 scale-95"
-                          enterTo="opacity-100 scale-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100 scale-100"
-                          leaveTo="opacity-0 scale-95"
-                        >
-                          <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                            <Dialog.Title
-                              as="h3"
-                              className="text-lg font-medium leading-6 text-gray-900 text-center"
-                            >
-                              {ser.name}
-                            </Dialog.Title>
-                            <div className="mt-2">
-                                {
-                                    
-                                    Object.keys(myarr[index]).map((item,index)=>{
-                                        return(
-                                           
-                                            <Image src={(item+2).img1}/>
-                                        )
-                                    })
-                                }
-                             
-                            </div>
-
-                            <div className="mt-4 text-center">
-                              <button
-                                type="button"
-                                className=" inline-flex justify-center px-4 py-2 text-sm text-white bg-red-700 border border-transparent rounded-md hover:bg-red-500 duration-300"
-                                onClick={closeModal}
-                              >
-                                Close
-                              </button>
-                            </div>
-                          </div>
-                        </Transition.Child>
-                      </div>
-                    </Dialog>
-                  </Transition>
                 </div>
               );
             })}
           </Slider>
         </div>
       </div>
+
+      {selectedIndex && 
+     
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-10 overflow-y-auto"
+            onClose={closeModal}
+          >
+            <div className="min-h-screen px-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0" />
+              </Transition.Child>
+
+              {/* This element is to trick the browser into centering the modal contents. */}
+              <span
+                className="inline-block h-screen align-middle"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                
+                <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl ">
+                  <div className="mt-2">
+                  
+                    <h2 className="py-2 uppercase text-white bg-red-700 text-center">
+                    {selectedIndex.name}
+                    </h2>
+                    
+                  </div>
+                  <div className="py-2"></div>
+                    <div className="grid sm:grid-cols-3 grid-cols-2 gap-2">
+                  {
+                    selectedIndex.img.map((item,index)=>{
+                      return(
+                        <div className="flex cursor-pointer" key={index}  onClick={()=>openModal2(item)}>
+                            <Image src={item}/>
+                        </div>
+                      )
+                    })
+                  }
+                  </div>
+
+                  <div className="mt-4 text-center">
+                    <button
+                      type="button"
+                      className=" inline-flex justify-center px-4 py-2 text-sm text-white bg-red-700 border border-transparent rounded-md hover:bg-red-500 duration-300"
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+              {zoomedInImage && 
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                
+                <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 ">
+                  <div className="py-2"></div>
+                    <div className="grid sm:grid-cols-1">
+                        <div className="flex cursor-pointer">
+                          {console.log(zoomedInImage)}
+                            <Image src={zoomedInImage}/>
+                        </div>
+                  </div>
+
+                  <div className="mt-4 text-center">
+                    <button
+                      type="button"
+                      className=" inline-flex justify-center px-4 py-2 text-sm text-white bg-red-700 border border-transparent rounded-md hover:bg-red-500 duration-300"
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+              }
+            </div>
+          </Dialog>
+        </Transition>
+}
     </div>
   );
 };
 
 export default Gallery;
-
-
-
